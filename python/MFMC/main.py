8e29946f886d09e33da35ed0b2cb6eba3f338992
LingmeiShen <selinma.f@gmail.com>
2017-03-31 11:07:47 -0400

    output simulated prices in excel

--- python/MFMC/main.py
+++ python/MFMC/main.py
@@ -1,12 +1,34 @@
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
-
+import csv
 import mfmc as mc
 
 if __name__ == '__main__':
     marketUniverse = mc.MarketUniverse()
     tickers = ['AAPL', 'NKE', 'GOOG']
     marketUniverse.initializeFromTickers(tickers)
+
+   # marketFactor=marketUniverse.tickerToMarketFactorDict['GOOG']
+    startDate=mc.datetime64FromYYYYMMDD('20120101')
+    endDate=mc.datetime64FromYYYYMMDD('20121231')
+    decay=0.97
+    #mfv=mc.MarketFactorVector(marketFactor, startDate, endDate, decay)
+
+    #create a Market
+    #tickerToMarketFactorDict={'GOOG':marketFactor}
+    market=marketUniverse.marketForAllTickers(startDate,endDate,decay)
+    #sim=market.simulated_returns_dict()
+
+    #write a large number of simulation to a csv file
+    with open('simulation.csv','w') as simOutputStream:
+        simWriter=csv.writer(simOutputStream,delimiter='\t',quoting=csv.QUOTE_NONE)
+        simWriter.writerow(market.tickers())
+        numSims=10000
+        for i in range(1,numSims+1):
+            if i%1000==0:
+                print'completed simulation #'+str(i)
+            sim=market.simulated_prices_list()
+            simWriter.writerow(['{0:.2f}'.format(price) for price in sim])
 
     print "end of program"
--- python/MFMC/mfmc.py
+++ python/MFMC/mfmc.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
 import math
+import collections
 
 DATE_TIME_COLUMN = 'date'
 CLOSE_COLUMN = 'close'
 RETURN_COLUMN = 'return'
 WEIGHTED_RETURN_COLUMN = 'weightedReturn'
+
 
 
 def datetime64FromYYYYMMDD(x):
@@ -21,14 +23,17 @@ class MarketFactor:
     def __init__(self, ticker_, data_):
         self.ticker = ticker_
         close = data_[CLOSE_COLUMN]
-        data_[RETURN_COLUMN] = np.log(close / close.shift(1))
-        data_ = data_.ix[1:]  # drop first row because it has NaN log return
+        data_[RETURN_COLUMN] = np.log(close / close.shift(1)) #create a new column(return)
+        data_ = data_.ix[1:]  # drop first row because it has NaN log return, ix means row, iy column
         self.data = data_  # [::-1] #reverse so that most recent data is first
         self.data.index = self.data[DATE_TIME_COLUMN]
 
     def __repr__(self):
         return 'MarketFactor(' + self.ticker + ',' + yyyymmdd(self.data.index[0]) \
                + ' to ' + yyyymmdd(self.data.index[-1]) + ',' + str(len(self.data)) + ' days)'
+
+    def mostRecentPrice(self):
+        return self.data[CLOSE_COLUMN][-1]
 
 
 class MarketFactorVector:
@@ -55,6 +60,9 @@ class MarketUniverse:
     def __init__(self):
         self.tickerToMarketFactorDict = {}
 
+    def __repr__(self):
+        return 'MarketUniver('+str(self.tickerToMarketFactorDict)+')'
+
     def initializeFromFileNames(self, dataFileNames):
         fileCount = 0
         columnNames = [DATE_TIME_COLUMN, 'time', 'open', 'high', 'low', CLOSE_COLUMN, 'volume']
@@ -74,3 +82,40 @@ class MarketUniverse:
 
     def initializeFromTickers(self, tickers):
         self.initializeFromFileNames(['table_' + ticker + '.csv' for ticker in tickers])
+
+    def marketForTickers(self,tickers,startDate,endDate,decay):
+        tickerToMarketFactorDict=collections.OrderedDict()
+        for ticker in tickers:
+            tickerToMarketFactorDict[ticker]=self.tickerToMarketFactorDict[ticker]
+        return Market(tickerToMarketFactorDict,startDate,endDate,decay)
+
+    def marketForAllTickers(self,startDate,endDate,decay):
+        return self.marketForTickers(self.tickerToMarketFactorDict.keys(),startDate,endDate,decay)
+
+class Market:
+    def __init__(self, tickerToMarketFactorDict,startDate,endDate,decay):
+        self.tickerToMarketFactorVectorDict=collections.OrderedDict()
+        self.numDays=None
+        self.currentPrices=[]
+        for ticker, marketFactor in tickerToMarketFactorDict.items():
+            mfv=MarketFactorVector(marketFactor, startDate, endDate, decay)
+            if not self.numDays:
+                self.numDays=len(mfv.weightedReturns)
+            self.tickerToMarketFactorVectorDict[ticker]=mfv
+            self.currentPrices.append(marketFactor.mostRecentPrice())
+
+    def simulated_returns_dict(self):
+        noise=np.random.normal(loc=0,scale=1,size=self.numDays)
+        tickerToReturnDict={}
+        for ticker,mfv in self.tickerToMarketFactorVectorDict.items():
+            tickerToReturnDict[ticker]=np.dot(mfv.weightedReturns,noise)
+        return tickerToReturnDict
+
+    def simulated_returns_list(self):
+        return self.simulated_returns_dict().values()
+
+    def simulated_prices_list(self):
+        return np.multiply(self.currentPrices,np.exp(self.simulated_returns_list()))
+
+    def tickers(self):
+        return self.tickerToMarketFactorVectorDict.keys()
